f9066e5aec83192807cfd38fe7bd877d7b5580db
Cicada <enter4p@gmail.com>
2025-05-23 17:09:21 +0300

    bugfix

--- start.sh
+++ start.sh
@@ -6,7 +6,10 @@ do
   sleep 15
   python manage.py shell
   sleep 5
-  echo from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '123')
+  echo from django.contrib.auth import get_user_model;
+  sleep 5
+  echo User = get_user_model()
+  echo User.objects.create_superuser('admin', 'admin@myproject.com', '123')
   sleep 5
 
 done
