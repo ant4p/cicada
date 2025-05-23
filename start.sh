04bbdc8cb12df36f08c9715022533b0a7726ef74
Cicada <enter4p@gmail.com>
2025-05-23 16:58:42 +0300

    optimization

--- start.sh
+++ start.sh
@@ -4,8 +4,13 @@ until  python manage.py migrate
 do
   echo "..."
   sleep 15
+  python manage.py shell
+  sleep 5
+  echo from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '123')
+  sleep 5
 
 done
+
 python manage.py collectstatic --noinput
 
 gunicorn --bind 0.0.0.0:8000 cicada.wsgi
