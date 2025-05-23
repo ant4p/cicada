6665219cfe56fbf10ff5f5c88d2ae1b5f9cd310b
Cicada <enter4p@gmail.com>
2025-05-23 17:56:17 +0300

    bugfix

--- start.sh
+++ start.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-until  python manage.py migrate
-do
+until [
   echo "..."
   sleep 15
+  python manage.py shell
+  sleep 5
+  echo "from django.contrib.auth import get_user_model"
+  sleep 5
+  echo "User = get_user_model()"
+  sleep 5
+  echo "User.objects.create_superuser('admin', 'admin@myproject.com', '123')"
+  sleep 5
+
+]; do python manage.py migrate
 
 done
+
 
 python manage.py collectstatic --noinput
 
