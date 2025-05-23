04bee008f7b877e2dc455748614073da91bf5e47
Cicada <enter4p@gmail.com>
2025-05-23 17:39:39 +0300

    bugfix

--- start.sh
+++ start.sh
@@ -4,14 +4,6 @@ until  python manage.py migrate
 do
   echo "..."
   sleep 15
-  python manage.py shell
-  sleep 5
-  echo "from django.contrib.auth import get_user_model"
-  sleep 5
-  echo "User = get_user_model()"
-  sleep 5
-  echo "User.objects.create_superuser('admin', 'admin@myproject.com', '123')"
-  sleep 5
 
 done
 
