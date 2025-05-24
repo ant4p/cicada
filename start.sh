2087ec02637c4f3061847fa3f26d56e7e510d966
Cicada <enter4p@gmail.com>
2025-05-24 12:47:11 +0300

    deploy settings

--- start.sh
+++ start.sh
@@ -12,12 +12,8 @@ done
 python manage.py shell
 
 sleep 5
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '123')"
-sleep 5
-echo "from services.models import Service; Service.objects.create(title='3D Моделирование', slug='3d-modelirovanie', content='123')"
-sleep 5
-echo "from services.models import Service; Service.objects.create(title='3D Визуализация', slug='3d-vizualizaciya', content='123')"
-sleep 5
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '123');
+
 
 python manage.py collectstatic --noinput
 
