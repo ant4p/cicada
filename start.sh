165014956c5e637ba0c34915fe20ba11a15e497b
Cicada <enter4p@gmail.com>
2025-05-24 13:08:21 +0300

    deploy settings

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-until python manage.py migrate
+
+until
+    python manage.py makemigrations
+    python manage.py migrate
 
 do
   echo "..."
@@ -12,7 +15,9 @@ done
 python manage.py shell
 
 sleep 5
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '123')"
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '123');
+from services.models import Service; Service.objects.create(title='3D Моделирование', slug='3d-modelirovanie', content='123');
+from services.models import Service; Service.objects.create(title='3D Визуализация', slug='3d-vizualizaciya', content='123')"
 
 
 python manage.py collectstatic --noinput
