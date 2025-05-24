6a8890cf1011d85835f916a4bfec1acd5a164d92
Cicada <enter4p@gmail.com>
2025-05-24 16:14:30 +0300

    deploy settings

--- start.sh
+++ start.sh
@@ -7,17 +7,19 @@ until
     python manage.py migrate
 
 do
-  echo "..."
+  echo "...prepare_database..."
   sleep 20
 
 done
 
-
 sleep 5
 
+if
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.get(is_superuser=True) != []" | python manage.py shell
 
+then
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '123')" | python manage.py shell
-
+fi
 
 python manage.py collectstatic --noinput
 
