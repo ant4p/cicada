3724ca8f4ee875907d4f94b051fbb89a403f1712
Cicada <enter4p@gmail.com>
2025-05-29 15:13:53 +0300

    big logic update

--- start.sh
+++ start.sh
@@ -16,12 +16,6 @@ sleep 5
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', '123')" | python manage.py shell
 
-echo "from services.models import Service; Service.objects.create(title='3D Моделирование', slug='3d-modelirovanie', content='123')" | python manage.py shell
-
-echo "from services.models import Service; Service.objects.create(title='3D Визуализация', slug='3d-vizualizaciya', content='123')" | python manage.py shell
-
-
-
 python manage.py collectstatic --noinput
 
 gunicorn --bind 0.0.0.0:8000 cicada.wsgi
