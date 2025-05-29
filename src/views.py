4b852d22d5361a64a79ea28f0b002130b9e2be70
Cicada <enter4p@gmail.com>
2025-05-29 16:01:37 +0300

    user agreement fixes

--- src/views.py
+++ src/views.py
@@ -31,8 +31,9 @@ class ShowUserAgreement(TemplateView):
     template_name = 'src/user_agreement.html'
 
     def get_context_data(self, **kwargs):
-        contex = super().get_context_data(**kwargs)
-        contex['agreement_data'] = UserAgreement.objects.all()
+        context = super().get_context_data(**kwargs)
+        context['agreement_data'] = UserAgreement.objects.all()
+        return context
 
 
 class ShowRobotsView(TemplateView):
