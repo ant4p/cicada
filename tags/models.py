c01e440c76d9086b8a75c334e423e595ffebdf3f
Cicada <enter4p@gmail.com>
2025-06-05 12:29:35 +0300

    bugfixes - slug

--- tags/models.py
+++ tags/models.py
@@ -5,8 +5,8 @@ from src.utils import generate_unique_slug
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=15, db_index=True, verbose_name='Заголовок')
-    slug = models.SlugField(max_length=16,db_index=True, unique=True, verbose_name="Slug")
+    title = models.CharField(max_length=50, db_index=True, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=55,db_index=True, unique=True, verbose_name="Slug")
 
     class Meta:
         db_table = 'tag'
