d5b96576176d8509310fc7a6ffe64c74fa2243dd
Cicada <enter4p@gmail.com>
2025-06-05 13:38:04 +0300

    pagination update

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,7 +5,7 @@ from portfolio.models import PortfolioItem
 class PortfolioListView(ListView):
     template_name = 'portfolio/portfolio_main.html'
     context_object_name = 'items'
-    paginate_by = 28
+    paginate_by = 32
 
     def get_queryset(self):
         return PortfolioItem.objects.all()
